3cd0ccf2d1bd9a6491c5655d986085dfb25a3a36
saidjonjalolov <bmwmf8896@gmail.com>
2024-03-18 16:18:22 +0500

    Logging Impliment

---
+++ Broker/logging/ILoggingBroker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.CRUD.Broker.logging
+{
+    internal interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogError(string UserMessage);
+    }
+}
---
+++ Broker/logging/LoggingBroker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.CRUD.Broker.logging
+{
+    internal class LoggingBroker : ILoggingBroker
+    {
+        public void LogError(string UserMessage)=>
+            Console.WriteLine(UserMessage);
+
+        public void LogInformation(string message)=>
+            Console.WriteLine(message);
+        
+    }
+}
