c2cb443e90aa2cd9adc8f9434e75bbe3f6fe1829
saidjonjalolov <bmwmf8896@gmail.com>
2024-03-28 21:42:34 +0500

    Cread Project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
 
 
 using System;
@@ -7,20 +7,35 @@ using Book.CRUD.Models;
 using Book.CRUD.Service;
 
 IBookService bookService = new BookService();
+//var book = bookService.GetBook(1);
+//Console.WriteLine($"{book.Id}. {book.Name} {book.Author}");
 bool isContinue = true;
 do
 {
     Console.WriteLine("1. Get All");
     Console.WriteLine("2. Add Book");
+    Console.WriteLine("3. Update one book");
     Console.Write("Enter command: ");
     string command = Console.ReadLine();
     if (command.Contains("2") is true)
     {
-        bookService.InsertBook(new Books() { Id = 3, Name = "Shaytanat", Author = "Toxir Malik" });
+        Books bookInfo = new Books();
+        Console.Write("Enter book id: ");
+        bookInfo.Id = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter book name: ");
+        bookInfo.Name = Console.ReadLine();
+        Console.Write("Enter book author: ");
+        bookInfo.Author = Console.ReadLine();
+
+        bookService.InsertBook(bookInfo);
     }
     if (command.Contains("1") is true)
     {
         bookService.ReadAllBook();
+    }
+    if (command.Contains("3") is true)
+    {
+        bookService.Update(2,new Books() {Id = 3, Name = "Soadat asri", Author = "Muhammadyusuf"});
     }
 
     Console.Write("Is Continue");
